a5d0794038cd15c32c86f5ae019520deaa159eaa
asystech-chen <127661237+asystech-chen@users.noreply.github.com>
2023-03-16 20:43:52 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,21 @@
+import math
+for i in range(100,200):
+    flag=0
+    for j in range(2,round(math.sqrt(i))+1):
+        if i%j==0:
+            flag=1
+            break
+    if flag:
+        continue
+    print(i)
+
+
+print('\nSimplify the code with "else"\n')
+
+
+for i in range(100,200):
+    for j in range(2,round(math.sqrt(i))+1):
+        if i%j==0:
+            break
+    else:
+        print(i)
